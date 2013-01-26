7478fa4e83e96d3b44500d9311cc3f36e0dfab31
Drew Griffith <wgriffith2@liberty.edu>
2013-01-25 22:07:33 -0500

    Minor touch ups

--- SF.R
+++ SF.R
@@ -1,18 +1,18 @@
-# install_github("toolbox", "drewgriffith15")
-# require(toolbox)
-
-# load packages
-load.packages('forecast,quantmod,TTR,PerformanceAnalytics,toolbox')
-
 ##########################################################################
+
+# LOAD PACKAGES
+install_github("toolbox", "drewgriffith15")
+require(toolbox)
+load.packages('forecast,quantmod,toolbox')
 
 # LOAD DATA
 ticker = toupper('SPY')
-data = getSymbols(ticker, src = 'yahoo', from = '1980-01-01', auto.assign = F)
+data = getSymbols(ticker, src='yahoo',from='1980-01-01', auto.assign=F)
 adj.close = coredata(Ad(data))
 dates = index(data)
 n.hist=45; n.fore=15
 
+# FIND MATCHES
 fm = find.matches(adj.close,n.hist,n.fore,model="linear", use.cd=FALSE)
 fit = lm( Y~. , data=fm$rmodel ) #http://bit.ly/WCiGpw
 summary(fit)
@@ -31,12 +31,12 @@ for (i in 1:n.match){
   adj=Ad(data)[(max.index[i] - n.hist + 1):max.index[i]]
   add_Series(adj, on=1)
 }
-add_Series(adj, on=1) #BUG?? This works though...
+add_Series(adj, on=1) #BUG?? SHOULDN'T NEED THIS...
 
+# BUILD FORECAST
 newdf = fm$fmodel
 forecast = forecast.lm(fit, newdata=newdf)
 forecast = forecast$mean
-
 y = as.xts(last(data[,6],180), 
            index(data)[(NROW(data)-(n.hist-1)):NROW(data)])
 z = extendForecast(dates, round(forecast,2))
@@ -54,3 +54,5 @@ add_BBands()
 
 # OUTPUT = PREVIOUS CLOSING + FORECAST
 tail(out,n.fore+1)
+
+##########################################################################
