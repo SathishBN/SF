885c2e8494ba209fb39e44dbce2750d91d87f2a5
Drew Griffith <wgriffith2@liberty.edu>
2015-01-14 15:39:58 -0500

    OK
    Adding VIX and Volume to the regression model

--- SF.R
+++ SF.R
@@ -10,45 +10,63 @@ close(con)
 library(griffun)
 load.packages('forecast,quantmod,svDialogs,lmtest,TTR')
 
-## INPUT BOX
-sym <- dlgInput("Enter Symbol: ", default="SPY")$res
-if (!length(sym)) { # The user clicked the cancel button
-  cat("OK, if you'd rather not, that is fine too...\n")
-} else {
-  cat("Gathing data for forecast on ", toupper(spl(sym)), "\n")
-}
+tickers = spl('SPY,^VIX')
 
-## ONLY SUPPORTS ONE STOCK CURRENTLY
-tickers = toupper(spl(sym))
 data <- new.env()
-getSymbols(tickers, src = 'yahoo', from = '1950-01-01', env = data, auto.assign = T)
-for(i in ls(data)) data[[i]] = adjustOHLC(data[[i]], use.Adjusted=T)
-quotes = getQuote(tickers)
-for(i in ls(data))
-  if( last(index(data[[i]])) < as.Date(quotes[i, 'Trade Time']) ) {
-    data[[i]] = rbind( data[[i]], make.xts(quotes[i, spl('Open,High,Low,Last,Volume,Last')],
-                                           as.Date(quotes[i, 'Trade Time'])))
-  }
-bt.prep(data)
-summary(data$prices)
+getSymbols(tickers, src = 'yahoo', from = '1980-01-01', env = data, auto.assign = T)
+for(i in data$symbolnames) data[[i]] = adjustOHLC(data[[i]], use.Adjusted=T)
+bt.prep(data, align='remove.na', fill.gaps = T)
 
-adj.close = data$prices
+# Closing VIX prices
+VIX = Cl(data$VIX)
+bt.prep.remove.symbols(data, 'VIX')
+
+# Closing SPY prices
+SPY = Cl(data$SPY)
+
+# Natural Log of Volume
+VOL = log(Vo(data$SPY))
+
+prices = SPY
 fm.dates = data$dates
 n.hist=35; n.fore=20
 
 ## BEGIN BACK TESTING --->
 
-bt.adj.close = adj.close[1:(NROW(adj.close)-n.fore)]
-bt.dates = data$dates[1:(NROW(adj.close)-n.fore)]
+bt.adj.close = SPY[1:(NROW(SPY)-n.fore)]
+bt.dates = data$dates[1:(NROW(SPY)-n.fore)]
 
 ## Find matches in historical data
-bt.ves.cd = find.matches(bt.adj.close,n.hist,n.fore,model="ves", use.cd=TRUE)
+bt.ves.cd = find.matches(bt.adj.close,n.hist,n.fore,model="ves", use.cd=TRUE,n.match=round(n.hist*.4)-5)
+# cor(bt.ves.cd$rmodel$Y,bt.ves.cd$rmodel$X1)
+
+# Using top matches, get VIX and Volume data and add it to the regression model
+RVX1 = data.frame(VIX[bt.ves.cd$matchindx[1]:(bt.ves.cd$matchindx[1]+n.hist-1)]); colnames(RVX1) = "VX1"
+lnVX1 = log(RVX1); colnames(lnVX1) = "lnVX1"
+
+RVX2 = data.frame(VIX[bt.ves.cd$matchindx[2]:(bt.ves.cd$matchindx[2]+n.hist-1)]); colnames(RVX2) = "VX2"
+lnVX2 = log(RVX2); colnames(lnVX2) = "lnVX2"
+
+VOLX1 = data.frame(VOL[bt.ves.cd$matchindx[1]:(bt.ves.cd$matchindx[1]+n.hist-1)]); colnames(VOLX1) = "VOLX1"
+
+bt.reg.data = cbind(bt.ves.cd$rmodel,RVX1,RVX2,lnVX1,lnVX2,VOLX1)
 
 ## Calculate regression model
-bt.ves.cd.fit = lm(Y~. , data=bt.ves.cd$rmodel)
+bt.ves.cd.fit = lm(Y~. , data=bt.reg.data)
+summary(bt.ves.cd.fit)
 
 ## Building forecast model
-bt.ves.cd.fcast = forecast.lm(bt.ves.cd.fit, newdata=bt.ves.cd$fmodel)
+FVX1 = data.frame(VIX[bt.ves.cd$matchindx[1]:(bt.ves.cd$matchindx[1]+n.fore-1)]); colnames(FVX1) = "VX1"
+lnFVX1 = log(FVX1); colnames(lnFVX1) = "lnVX1"
+
+FVX2 = data.frame(VIX[bt.ves.cd$matchindx[2]:(bt.ves.cd$matchindx[2]+n.fore-1)]); colnames(FVX2) = "VX2"
+lnFVX2 = log(FVX2); colnames(lnFVX2) = "lnVX2"
+
+FVOLX1 = data.frame(VOL[bt.ves.cd$matchindx[1]:(bt.ves.cd$matchindx[1]+n.fore-1)]); colnames(FVOLX1) = "VOLX1"
+
+bt.fcast.data = cbind(bt.ves.cd$fmodel,FVX1,FVX2,lnFVX1,lnFVX2,FVOLX1)
+
+bt.ves.cd.fcast = forecast.lm(bt.ves.cd.fit, newdata=bt.fcast.data)
 bt.ves.cd.fcast = bt.ves.cd.fcast$mean
 bt.ves.cd.forecast = extendForecast(bt.dates, round(bt.ves.cd.fcast,4))
 colnames(bt.ves.cd.forecast) = "bt.ves.cd.FORECAST"
@@ -58,7 +76,7 @@ bt.ag.forecast = extendForecast(bt.dates,bt.ves.cd.forecast)
 colnames(bt.ag.forecast) = "FORECAST"
 
 ## What really happened...
-hist.adj.close = adj.close[(NROW(adj.close)-(n.fore-1)):NROW(adj.close)]
+hist.adj.close = prices[(NROW(prices)-(n.fore-1)):NROW(prices)]
 colnames(hist.adj.close) = "HISTORY"
 
 ## Quick comparison
@@ -76,18 +94,38 @@ bt.model.acc = acc(bt.out$FORECAST, bt.out$HIST) ## Overall Accuracy
 bt.model.cor = cor(bt.out$FORECAST,bt.out$HIST) ## Correlation
 bt.model.acc;bt.model.cor
 
-## <--- END BACK TESTING
-
 ## BEGIN FORECAST MODEL --->
 
 ## Find matches to create regression model
-fm.ves.cd = find.matches(adj.close,n.hist,n.fore,model="ves", use.cd=TRUE)
+fm.ves.cd = find.matches(prices,n.hist,n.fore,model="ves", use.cd=TRUE, n.match=round(n.hist*.25))
+
+# Using top matches, get VIX and Volume data and add it to the regression model
+RVX1 = data.frame(VIX[fm.ves.cd$matchindx[1]:(fm.ves.cd$matchindx[1]+n.hist-1)]); colnames(RVX1) = "VX1"
+lnVX1 = log(RVX1); colnames(lnVX1) = "lnVX1"
+
+RVX2 = data.frame(VIX[fm.ves.cd$matchindx[2]:(fm.ves.cd$matchindx[2]+n.hist-1)]); colnames(RVX2) = "VX2"
+lnVX2 = log(RVX2); colnames(lnVX2) = "lnVX2"
+
+VOLX1 = data.frame(VOL[fm.ves.cd$matchindx[1]:(fm.ves.cd$matchindx[1]+n.hist-1)]); colnames(VOLX1) = "VOLX1"
+
+fm.reg.data = cbind(fm.ves.cd$rmodel,RVX1,RVX2,lnVX1,lnVX2,VOLX1)
 
 ## Calculate regression model
-fm.ves.cd.fit = lm(Y~. , data=fm.ves.cd$rmodel)
+fm.ves.cd.fit = lm(Y~. , fm.reg.data)
+summary(fm.ves.cd.fit)
 
 ## Building forecast model
-fm.ves.cd.fcast = forecast.lm(fm.ves.cd.fit, newdata=fm.ves.cd$fmodel)
+FVX1 = data.frame(VIX[fm.ves.cd$matchindx[1]:(fm.ves.cd$matchindx[1]+n.fore-1)]); colnames(FVX1) = "VX1"
+lnFVX1 = log(FVX1); colnames(lnFVX1) = "lnVX1"
+
+FVX2 = data.frame(VIX[fm.ves.cd$matchindx[2]:(fm.ves.cd$matchindx[2]+n.fore-1)]); colnames(FVX2) = "VX2"
+lnFVX2 = log(FVX2); colnames(lnFVX2) = "lnVX2"
+
+FVOLX1 = data.frame(VOL[fm.ves.cd$matchindx[1]:(fm.ves.cd$matchindx[1]+n.fore-1)]); colnames(FVOLX1) = "VOLX1"
+
+fm.fcast.data = cbind(fm.ves.cd$fmodel,FVX1,FVX2,lnFVX1,lnFVX2,FVOLX1)
+
+fm.ves.cd.fcast = forecast.lm(fm.ves.cd.fit, newdata=fm.fcast.data )
 fm.ves.cd.fcast = fm.ves.cd.fcast$mean
 fm.ves.cd.forecast = extendForecast(fm.dates, round(fm.ves.cd.fcast,4))
 colnames(fm.ves.cd.forecast) = "fm.ves.cd.FORECAST"
@@ -96,41 +134,36 @@ colnames(fm.ves.cd.forecast) = "fm.ves.cd.FORECAST"
 n.match = NROW(fm.ves.cd$matchindx)
 max.index = fm.ves.cd$matchindx
 d.matches = index(fm.dates)[1:NROW(fm.dates)]
-plota(adj.close, type='l', col='gray', main=tickers)
-plota.lines(last(adj.close,n.hist), col='blue')
+plota(prices, type='l', col='gray', main="SPY")
+plota.lines(last(prices,n.hist), col='blue')
 for(i in 1:n.match) {
-  plota.lines(adj.close[max.index[i]:(max.index[i]+n.hist)], col='red')
+  plota.lines(prices[max.index[i]:(max.index[i]+n.hist)], col='red')
 }
 
 ## Putting it all together
-y = as.xts(last(adj.close,180),
+y = as.xts(last(prices,180),
            index(fm.dates)[(NROW(fm.dates)-(n.hist-1)):NROW(fm.dates)])
 z = extendForecast(fm.dates,fm.ves.cd.forecast)
 fm.ag.forecast = rbind(y,z)
-colnames(fm.ag.forecast) = "Adj.Close"
+colnames(fm.ag.forecast) = "Close"
 
 ## Quick technical analysis
 thm = chart_theme()
 thm$col$line.col = 'blue'
-chart_Series(fm.ag.forecast, theme=thm,name=tickers)
+chart_Series(fm.ag.forecast, theme=thm,name="SPY")
 add_Series(last(fm.ag.forecast,(n.fore+1)),on=1)
 add_RSI(n=14)
 add_BBands()
 
-## Market Price
-mark = round(last(adj.close),2); colnames(mark) = "Adj.Close"
-# mark$Buy.Sell = 0
-
 ## Future Prices
 future = tail(fm.ag.forecast, n.fore)
-future = rbind(mark, future)
-future$Buy.Sell = buy.sell(future$Adj.Close)$Buy.Sell
+future$Buy.Sell = buy.sell(future$Close)$Buy.Sell
 
 ## Model specs
-profit = sum(buy.sell(future$Adj.Close)$Buy.Sell*(-future$Adj.Close))
+profit = sum(buy.sell(future$Close)$Buy.Sell*(-future$Close))
 model.acc = acc(fm.ves.cd$rmodel$Y,fm.ves.cd.fit$fitted.values) ## Model Accuracy
 ur2 = unadj.rsquared(fm.ves.cd.fit)$unadj.rsquared
-yr.return = last(yearlyReturn(adj.close),1)
+yr.return = last(yearlyReturn(prices),1)
 
 ## <--- END FORECAST MODEL
 
