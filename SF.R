1d4a78f4ed644568e2d23d8def2bd32ad6bb7ac7
Drew Griffith <wgriffith2@liberty.edu>
2015-07-17 13:09:08 -0400

    OK

--- SF.R
+++ SF.R
@@ -1,7 +1,9 @@
 # References: Predictability of the daily high and low of the S&P 500 index by Clive Jones
+# Method is called EVPA for Extreme Value Prediction Algorithm
+# Predicts the high and low price for the upcoming week for SPY
+# Suggested to run time for this script is Monday - 9:50 AM EST 
 
 library(griffun)
-
 load.packages('forecast,quantmod,svDialogs,lmtest,TTR')
 
 # Disable Sci Notation and supress warnings
@@ -25,7 +27,6 @@ summary(data$prices)
 ## BEGIN BACKTESTING AND FORECAST--->
 
 ## Forecast will include last n days plus the current day
-
 SPY <- data$SPY
 SPY <- to.weekly(data$SPY)
 indx <- round(nrow(SPY)*0.8) # grab the last n% of the dataset
@@ -61,7 +62,6 @@ bt.high.fit <- lm(Y~. , data=bt.high.reg.data); summary(bt.high.fit)
 bt.low.fit <- lm(Y~. , data=bt.low.reg.data); summary(bt.low.fit)
 
 ## Building forecast model
-
 bt.high.fcast.data <- tail(SPY[,9:12],bt)
 colnames(bt.high.fcast.data) <- c("X1","X2","X3","X4")
 
@@ -72,17 +72,40 @@ bt.high.fcast <- forecast.lm(bt.high.fit, newdata=bt.high.fcast.data)
 bt.low.fcast <- forecast.lm(bt.low.fit, newdata=bt.low.fcast.data)
 
 # Forecast Results
-
 bt.high.fcast <- as.xts(cbind(data.frame(tail(Hi(SPY),bt)),data.frame(tail(HiLag1,bt)),data.frame(bt.high.fcast$mean)))
 colnames(bt.high.fcast) <- c("Hi","PH","HighGrowth")
 bt.high.fcast$Forecast.High <- (1+bt.high.fcast$HighGrowth) * bt.high.fcast$PH
-bt.high.fcast; acc(bt.high.fcast$Hi,bt.high.fcast$Forecast.High)
+bt.high.fcast
+bt.high.acc <- round(acc(bt.high.fcast$Hi,bt.high.fcast$Forecast.High),3) * 100
 
 bt.low.fcast <- as.xts(cbind(data.frame(tail(Lo(SPY),bt)),data.frame(tail(LoLag1,bt)),data.frame(bt.low.fcast$mean)))
 colnames(bt.low.fcast) <- c("Lo","PL","LowGrowth")
 bt.low.fcast$Forecast.low <- (1+bt.low.fcast$LowGrowth) * bt.low.fcast$PL
-bt.low.fcast; acc(bt.low.fcast$Lo,bt.low.fcast$Forecast.low)
+bt.low.fcast
+bt.low.acc <- round(acc(bt.low.fcast$Lo,bt.low.fcast$Forecast.low),3) * 100
 
-# last(bt.high.fcast[,4]); last(bt.low.fcast[,4])
+current.price <- round(last(SPY[,1]),2)
+forecast.high <- round(last(bt.high.fcast[,4]),2)
+forecast.low <- round(last(bt.low.fcast[,4]),2)
 
-## <--- END BACKTESTING AND (N) PERIOD FORECAST
+# Send to Pushbullet
+library(httr)
+setwd("/Users/wgriffith2/Dropbox/R/API")
+wd <- getwd()
+
+# NEVER put credentials or api keys in script bodies!!
+# Get API key from file location
+wag.api.key<-readLines(paste0(wd,"/wag.rpushbullet.json"))
+jdr.api.key<-readLines(paste0(wd,"/jdr.rpushbullet.json"))
+
+push.text <- paste0("Currently: $",current.price,"    Forecast: $",
+                    forecast.high," - $",forecast.low,"    Accuracy (20w): ",
+                    bt.high.acc, "% - ", bt.low.acc, "%")
+
+# Send note through Pushbullet
+api <- "https://api.pushbullet.com/api/pushes"
+note <- list(type='note', title='SPY Weekly Forecast', body=push.text)
+invisible(POST(api, authenticate(wag.api.key, ""), body=note))
+invisible(POST(api, authenticate(jdr.api.key, ""), body=note))
+
+## <--- END
---
+++ SF.bat
@@ -0,0 +1,1 @@
+"C:\Program Files\R\R-3.2.0\bin\R.exe" CMD BATCH --vanilla --slave "C:\Users\wgriffith2\Dropbox\R\SF\SF.R"
